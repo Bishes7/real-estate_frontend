dfc38d361218bf25cd808ff30058b8dc93ae648b
Bishes Adhikari <specialbishes9@gmail.com>
2025-08-06 17:41:00 +1000

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-function App() {
-  return <h1 className="btn btn-success">button</h1>;
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+("./pages/Home");
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
