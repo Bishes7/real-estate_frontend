0973de47fe3708fc753e758897526365f9f9f933
Bishes Adhikari <specialbishes9@gmail.com>
2025-08-17 23:52:36 +1000

    completed property landing page

--- src/pages/PropertyLandingPage.jsx
+++ src/pages/PropertyLandingPage.jsx
@@ -5,35 +5,116 @@ import { Loader } from "../components/ui/Loader";
 import { Message } from "../components/ui/Message";
 import FormContainer from "../components/FormContainer";
 import { BASE_URL } from "../utils/constants";
+import { FaBed, FaBath, FaCar, FaCouch } from "react-icons/fa";
+
+// Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const PropertyLandingPage = () => {
   const { listingId } = useParams();
-  const {
-    data: listing,
-    isLoading,
-    refetch,
-    error,
-  } = useGetListingQuery(listingId);
+  const { data: listing, isLoading, error } = useGetListingQuery(listingId);
 
   if (isLoading) return <Loader />;
   if (error) return <Message variant="danger">{error}</Message>;
 
   return (
-    <FormContainer>
-      <h3>{listing.name}</h3>
-      <p>{listing.description}</p>
-      <p>
-        <strong>Address:</strong>
-        {listing.address}
-      </p>
+    <div>
+      {/* Title */}
+
+      {/* Swiper Carousel */}
       {listing.images?.length > 0 && (
-        <img
-          src={`${BASE_URL}${listing.images[0]}`}
-          alt={listing.name}
-          style={{ width: "400px", borderRadius: "10px" }}
-        />
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          loop={true}
+          spaceBetween={10}
+          slidesPerView={1}
+          style={{
+            width: "100vw",
+            height: "60vh",
+          }}
+        >
+          {listing.images.map((img, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={`${BASE_URL}${img}`}
+                alt={`${listing.name}-${index}`}
+                style={{
+                  width: "100%",
+                  height: "500px",
+                  objectFit: "cover",
+                }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       )}
-    </FormContainer>
+
+      {/* Property Info */}
+      <FormContainer>
+        <div className="p-4">
+          {/* Title + Price */}
+          <h3 className="fw-bold mb-2">
+            {listing.name} - $
+            {listing.offer && listing.discountedPrice > 0
+              ? listing.discountedPrice
+              : listing.regularPrice}{" "}
+            {listing.type === "rent" && "/ month"}
+          </h3>
+          {/* Status + Offer Badges */}
+          <div className="d-flex gap-2 mb-3">
+            <span
+              className={`badge px-4 py-2 ${
+                listing.type === "rent" ? "bg-danger" : "bg-success"
+              }`}
+              style={{ fontSize: "1rem" }}
+            >
+              {listing.type === "rent" ? "For Rent" : "For Sale"}
+            </span>
+
+            <span
+              className="badge bg-success px-4 py-2"
+              style={{ fontSize: "1rem" }}
+            >
+              $
+              {listing.offer && listing.discountedPrice > 0
+                ? listing.discountedPrice
+                : listing.regularPrice}
+            </span>
+          </div>
+          {/* Description */}
+          <strong>Description: </strong> {listing.description}
+          {/* Address */}
+          <p>
+            <strong>Address:</strong> {listing.address}
+          </p>
+          <div className="d-flex gap-4 flex-wrap mt-3 ">
+            <span className="d-flex align-items-center gap-1 ">
+              <FaBed />
+              {listing.beds} Beds
+            </span>
+            <span className="d-flex align-items-center gap-1">
+              <FaBath />
+              {listing.baths} Baths
+            </span>
+            <span className="d-flex align-items-center gap-1">
+              <FaCar />
+              {listing.parking ? "Parking" : "No Parking"}
+            </span>
+            <span className="d-flex align-items-center gap-1">
+              <FaCouch />
+              {listing.furnished ? "Furnished" : "Not furnished"}
+            </span>
+          </div>
+        </div>
+      </FormContainer>
+    </div>
   );
 };
 
